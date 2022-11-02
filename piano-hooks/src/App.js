c9bf87f68418d9f9987d023d47ac9b435a3728c9
rachel <jrachelr1@gmail.com>
2022-11-02 08:46:22 -0700

    adding to git

---
+++ piano-hooks/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Piano from './Piano';
+import React, { useEffect, useState } from "react";
+
+
+function App() {
+
+  return (
+    <div className="app-container">
+      <Piano />
+    </div>
+
+  );
+}
+
+export default App;
